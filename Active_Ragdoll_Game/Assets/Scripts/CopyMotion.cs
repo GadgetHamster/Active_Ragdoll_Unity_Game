36e9237be8f3a5be61433f27cc7e61d1c2c02609
GadgetHamster <77999353+GadgetHamster@users.noreply.github.com>
2021-02-07 13:42:07 -0800

    Added active ragdoll
    it work. animations work.

---
+++ Active_Ragdoll_Game/Assets/Scripts/CopyMotion.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyMotion : MonoBehaviour
+{
+  public Transform targetLimb;
+  public bool mirror;
+  ConfigurableJoint cj;
+  public bool flipZ;
+  public bool flipW;
+  Quaternion startRot;
+  Quaternion newRot;
+    // Start is called before the first frame update
+    void Start()
+    {
+     startRot =transform.localRotation;
+      //  startRot = Quaternion.identity;
+      cj = GetComponent<ConfigurableJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //Quaternion Output;
+      if (!mirror)
+      {
+        cj.targetRotation = targetLimb.localRotation * startRot;
+      //  Debug.Log(targetLimb.localEulerAngles);
+      }
+      else
+      {
+        cj.targetRotation = Quaternion.Inverse(targetLimb.localRotation) * startRot;
+      }
+
+
+    }
+}
